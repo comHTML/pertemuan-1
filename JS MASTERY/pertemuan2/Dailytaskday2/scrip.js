c076c53832d8a95c1a2b13379ed708246a7ce704
comHTML <raihannuric21@gmail.com>
2025-10-31 20:42:25 +0700

    first commit

---
+++ JS MASTERY/pertemuan2/Dailytaskday2/scrip.js
@@ -0,0 +1,35 @@
+let nama ="ini string"; //string
+let nama1 = 100; //integer / number
+let nama2 = false; //boolean
+let nama3 = null; //null
+let nama4 = undefined; //undefined
+
+const nama5 =""; //string
+const nama6 = 50; //integer
+const nama7 = true; //boolean
+const nama8 = null; //null
+// const undefined;
+
+var nama9 =""; //string
+var nama10 = 25; //number
+var nama11 = false; //boolean
+var nama12 = null; //null
+var nama13 = undefined //undefined
+
+let a = 5
+let b =5
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a==b)
+console.log(a===b)
+console.log(a!=b)
+console.log(a!==b)
+console.log(a>b)
+console.log(a<b)
+console.log(a>=b)
+console.log(a<=b)
+
+
---
+++ JS MASTERY/pertemuan2/Dailytaskday2/script2.js
@@ -0,0 +1,10 @@
+let nama123 = "sabun"
+let nama45 = "minyak"
+let a = 5
+let b = 10
+console.log ("harga " + nama45 + " adalah " + b );
+console.log ("harga " + nama123 + " adalah " + a );
+
+let result = a + b
+
+console.log ("total harga untuk produk: " + result);
